b727a6c9e389aebf4aabf39d1c0d9a3834f1e8a7
rexsot <112843175+rexsot@users.noreply.github.com>
2022-11-10 16:50:31 +0900

    x 좌표 계산

--- my_game/mygame.py
+++ my_game/mygame.py
@@ -5,8 +5,8 @@ import play_state
 #import play_state
 #import item_state
 
-X_MAX = 1920
-Y_MAX = 1080
+X_MAX = 910
+Y_MAX = 540
 
 pico2d.open_canvas(X_MAX, Y_MAX)
 game_framework.run(play_state) #logo_state
