3a52ca1a630b12219881f991c4921568e36e70ac
donghwanoh <112843175+rexsot@users.noreply.github.com>
2022-11-21 15:15:26 +0900

    v-1.0

--- new_game/player.py
+++ new_game/player.py
@@ -1,5 +1,15 @@
 from pico2d import *
 
+# up/down - 상하좌우 및 스페이스(점프), down - 대시(x)
+RIGHT_KEY_DOWN, LEFT_KEY_DOWN, UP_KEY_DOWN, DOWN_KEY_DOWN = range(4)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RD,
+    (SDL_KEYDOWN, SDLK_LEFT): LD,
+    (SDL_KEYUP, SDLK_RIGHT): RU,
+    (SDL_KEYUP, SDLK_LEFT): LU
+}
+
 class RUN:
     @staticmethod
     def enter():
